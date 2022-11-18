47d1c4c7df3b89bf7003aba7d9dc81e2c31aa6b1
Никита Есипов <dasaderto@gmail.com>
2022-11-18 21:03:24 +0300

    Fix project deps

--- backend/atha/settings.py
+++ backend/atha/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v%&c)ai2&^+s_#j8wbh$few@df6ms*u4380yfl_s*7@8demfp_'
+SECRET_KEY = os.environ.get('SECRET_KEY', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG')))
 
 ALLOWED_HOSTS = []
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
