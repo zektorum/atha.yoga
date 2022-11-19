417d2a7d4d76efdc5bcd427033f448612b3a5474
Никита Есипов <dasaderto@gmail.com>
2022-11-19 14:00:36 +0300

    Fix django project folders naming

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,7 +1,7 @@
-"""atha URL Configuration
+"""server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path
 
