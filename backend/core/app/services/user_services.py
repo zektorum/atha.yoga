b6ffe838968ec97f05a2341cd6ae51f3e1227d64
Никита Есипов <dasaderto@gmail.com>
2022-11-22 13:24:04 +0300

    Add user repository

--- backend/core/app/services/user_services.py
+++ backend/core/app/services/user_services.py
@@ -1,11 +1,14 @@
 from functools import cached_property
 from typing import Tuple
 
+from core.app.repositories.user_repository import UserRepository
 from core.app.services.types import UserRegisterData
 from core.models import User, get_tokens_for_user, UserToken
 
 
 class UserRegister:
+    repository = UserRepository()
+
     def __init__(self, data: UserRegisterData):
         self._data = data
 
@@ -14,7 +17,7 @@ class UserRegister:
         user = User()
         user.username = user.email = self._data["email"]
         user.set_password(self._data["password"])
-        user.save()
+        self.repository.store(user=user)
         return user
 
     def register(self) -> Tuple[User, UserToken]:
