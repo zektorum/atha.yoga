79832d79b308a20ec5cc1bafeef46c63b7ea3cd6
bloodeRok <blooder-12_123@mail.ru>
2022-11-24 20:15:34 +0300

    add switch password feature

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from core.app.handlers.user_handlers import UserRegisterHandler, UserLoginHandler
+from core.app.handlers.user_handlers import UserRegisterHandler, UserLoginHandler, UserSwitchPassHandler
 
 
 urlpatterns = [
     path("register/", UserRegisterHandler.as_view(), name="registration"),
     path("login/", UserLoginHandler.as_view(), name="login"),
+    path("switchpass/", UserSwitchPassHandler.as_view(), name="switchpass"),
 ]
