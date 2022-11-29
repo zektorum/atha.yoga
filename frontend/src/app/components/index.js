f5242c596b17db70f0413a42f9357a6893782c12
Xenia Gorbunova <Gorbunovaksenia@gmail.com>
2022-11-29 20:05:13 +0300

    Frontend. Logo компонент, первый экран.

---
+++ frontend/src/app/components/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+
+const Logo = () => (
+  <div className="logo__container">
+    <img src={logo} alt="logo-image" />
+  </div>
+);
+
+export default Logo;
