cb008a1c6918d331011e1917e7f6021be53f9dbe
Xenia Gorbunova <Gorbunovaksenia@gmail.com>
2022-11-29 20:57:08 +0300

    WelcomePage changes

--- frontend/src/app/pages/welcome/index.jsx
+++ frontend/src/app/pages/welcome/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const WelcomePage = () => (
+const WelcomePage = () => {
+  const [isLogo, setIsLogo] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLogo(false), 2000);
+  }, [])
+
+  return isLogo ? <Logo /> : <SplashScreens />;
+}
+  
+(
   <div>
     <h2>Splash screens</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At enim excepturi facere illo in ipsum itaque maxime modi natus nulla perferendis, porro quam quas repudiandae rerum sapiente, soluta voluptate voluptates!</p>
