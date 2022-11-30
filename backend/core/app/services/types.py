3792b63b785989bf94425bad7b970326018aeba1
bloodeRok <blooder-12_123@mail.ru>
2022-11-30 13:07:11 +0300

    Resolve conflict in types.py

--- backend/core/app/services/types.py
+++ backend/core/app/services/types.py
@@ -10,15 +10,18 @@ class UserLoginData(TypedDict):
     email: str
     password: str
 
+
 class UserChangePassData(TypedDict):
     email: str
     password: str
     new_password: str
 
+
 class TextMailData(TypedDict):
     subject: str
     message: str
     receivers: list
+
 
 class HtmlMailData(TypedDict):
     subject: str
