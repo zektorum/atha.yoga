9b0da317019e121c03cc7ad514a165c2e08bf106
Никита Есипов <dasaderto@gmail.com>
2022-11-30 23:49:20 +0300

    Add user roles

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -13,6 +15,16 @@ class TimeStampedModel(models.Model):
         ordering = ["-id"]
 
 
+class UserRoles(models.TextChoices):
+    STUDENT = "STUDENT"
+    TEACHER = "TEACHER"
+    MODERATOR = "MODERATOR"
+
+
+def user_default_roles() -> List[Union[UserRoles, str]]:
+    return [UserRoles.STUDENT]
+
+
 class User(AbstractUser):
     first_name = models.CharField(_("first name"), max_length=100)
     last_name = models.CharField(_("last name"), max_length=100)
@@ -21,3 +33,15 @@ class User(AbstractUser):
     about = models.CharField(max_length=100, blank=True)
     avatar = models.ImageField(upload_to="user_avatars/", blank=True)
     is_teacher = models.BooleanField(default=False)
+    roles = models.JSONField(default=user_default_roles)
+
+    def has_role(self, role: UserRoles) -> bool:
+        return role in self.roles
+
+    def add_roles(self, roles: List[UserRoles]) -> None:
+        self.roles = list({*self.roles, *roles})
+
+    def remove_role(self, role: UserRoles) -> None:
+        if not self.has_role(role=role):
+            return
+        del self.roles[self.roles.index(role)]
