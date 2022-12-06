61e3eedd14e59696817bd6684b1e86c10356975c
foliageh <mygazztop1@gmail.com>
2022-12-06 23:32:26 +0300

    configure admin panel

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,4 +1,29 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_teacher",
+        "roles",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+    list_display_links = ("id", "username", "email")
+    list_filter = (
+        "is_teacher",
+        "last_login",
+        "date_joined",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+    search_fields = ("username", "email", "first_name", "last_name")
+    raw_id_fields = ("groups", "user_permissions")
