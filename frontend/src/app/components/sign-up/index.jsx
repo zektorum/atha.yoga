0437e7dcec0b50cf1f840c4b7272a7ecfcbd4229
Xenia <GorbunovaKsenia@gmail.com>
2022-12-15 17:56:09 +0300

    links style improved

--- frontend/src/app/components/sign-up/index.jsx
+++ frontend/src/app/components/sign-up/index.jsx
@@ -48,6 +48,10 @@ const SignUp = () => {
     context.register({ email: data.get('email'), password: data.get('password') });
   };
 
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
   const MyFormHelperText = () => {
     const { focused } = useFormControl() || {};
 
@@ -62,104 +66,123 @@ const SignUp = () => {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
+  <Container component="main" maxWidth="xs">
+  <Box
+    sx={{
+      marginTop: 8,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}
+  >
+    <Typography component="h1" variant="h4" fontWeight="500" sx={{ mb: 3 }}>
+      Регистрация
+    </Typography>
+    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} className="form__container">
+      <TextField
+        sx={{ mb: 2 }}
+        margin="normal"
+        fullWidth
+        id="email"
+        label="Электронная почта"
+        placeholder="E-mail"
+        name="email"
+        autoComplete="email"
+        error={!!message?.email}
+        helperText={message?.email}
+        onFocus={() => handleFocus('email')}
+        autoFocus
+      />
+      <FormControl fullWidth variant="outlined" sx={{ mb: 2 }}>
+      <InputLabel>Пароль</InputLabel>
+      <OutlinedInput
+        fullWidth
+        label="Пароль"
+        name="password"
+        placeholder="Пароль"
+        id="password"
+        autoComplete="current-password"
+        type={values.showPassword ? 'text' : 'password'}
+        error={!!message?.password}
+        helperText={message?.password}
+        onFocus={() => handleFocus('password')}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+            >
+              {values.showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+      <MyFormHelperText />
+      </FormControl>
+      <Button
+        type="submit"
+        size="large"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
       >
-        <Typography component="h1" variant="h4" fontWeight="500" sx={{ mb: 3 }}>
-          Регистрация
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} className="form__container">
-          <TextField
-            sx={{ mb: 2 }}
-            margin="normal"
-            fullWidth
-            id="email"
-            label="Электронная почта"
-            placeholder="E-mail"
-            name="email"
-            autoComplete="email"
-            error={!!message?.email}
-            helperText={message?.email}
-            onFocus={() => handleFocus('email')}
-            autoFocus
-          />
-          <FormControl fullWidth variant="outlined" sx={{ mb: 2 }}>
-          <InputLabel>Пароль</InputLabel>
-          <OutlinedInput
-            fullWidth
-            label="Пароль"
-            name="password"
-            placeholder="Пароль"
-            id="password"
-            autoComplete="current-password"
-            type={values.showPassword ? 'text' : 'password'}
-            error={!!message?.password}
-            helperText={message?.password}
-            onFocus={() => handleFocus('password')}
-            InputProps={{
-              endAdornment:
-                (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                    >
-                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-            }}
-          />
-          <MyFormHelperText />
-          </FormControl>
-          <Button
-            type="submit"
-            size="large"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Зарегистрироваться
-          </Button>
-          <Grid container spacing={1} alignItems="center" justifyContent="center">
-            <Grid item>
-              <Typography variant="body2">
-                Уже есть аккаунт?
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Link to="/login" variant="body2" underline="none">
-                Войти
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-        <div style={{ position: 'absolute', bottom: 32 }}>
-          <Typography variant="caption">
-            Нажимая на кнопку «Зарегистрироваться», я подтверждаю,
+        Зарегистрироваться
+      </Button>
+      <Grid container spacing={1} alignItems="center" justifyContent="center">
+        <Grid item>
+          <Typography variant="body2">
+            Уже есть аккаунт?
           </Typography>
-          <Grid container justifyContent="center" spacing={1}>
-            <Grid item>
-              <Typography variant="caption">
-                что ознакомлен(а) с
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Link variant="caption" underline="none">
-                пользовательским соглашением
-              </Link>
-            </Grid>
-          </Grid>
-        </div>
-      </Box>
-    </Container>
-  );
+        </Grid>
+        <Grid item>
+          <Typography
+              component={Link}
+              to="/login"
+              variant="body2"
+              sx={{ textDecoration: 'none' }}
+          >
+            Войти
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+    <div style={{
+      position: 'absolute',
+      bottom: 32,
+      textAlign: 'center',
+      maxWidth: 380,
+      lineHeight: 0.1,
+    }}
+    >
+      <Typography variant="caption">
+        Нажимая на кнопку «Зарегистрироваться», вы принимаете условия
+        <Typography
+            component={Link}
+            variant="caption"
+            to="#"
+            sx={{ textDecoration: 'none' }}
+            marginLeft={1}
+        >
+          Пользовательского соглашения
+        </Typography>
+      </Typography>
+      <Typography variant="caption" sx={{ maxWidth: 300 }} marginLeft={1}>
+        и
+        <Typography
+            component={Link}
+            variant="caption"
+            to="#"
+            sx={{ textDecoration: 'none' }}
+            marginLeft={1}
+        >
+          Политики конфиденциальности
+        </Typography>
+      </Typography>
+    </div>
+  </Box>
+</Container>
+);
 };
-
 export default SignUp;
