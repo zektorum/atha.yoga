542cbde85316251276e4eb9e51c25764c51d5fbd
foliageh <mygazztop1@gmail.com>
2022-12-27 22:51:20 +0300

    fix prefetch

--- backend/courses/app/repositories/lesson_repository.py
+++ backend/courses/app/repositories/lesson_repository.py
@@ -10,17 +10,18 @@ from courses.models import Lesson
 class LessonRepository(BaseRepository):
     model = Lesson
 
+    def fetch_relations(self, lessons: QuerySet[Lesson]) -> QuerySet[Lesson]:
+        return lessons.prefetch_related("participants")
+
     def bulk_create(self, objs: List[Lesson]) -> None:
         self.model.objects.bulk_create(objs)
 
     def find_by_id(self, id_: int) -> Optional[Lesson]:
-        return (
-            self.model.objects.prefetch_related("participants").filter(pk=id_).first()
-        )
+        return self.model.objects.filter(pk=id_).first()
 
     def find_by_course_id(self, course_id: int) -> QuerySet[Lesson]:
-        return self.model.objects.prefetch_related("participants").filter(
-            course_id=course_id
+        return self.fetch_relations(
+            lessons=self.model.objects.filter(course_id=course_id)
         )
 
     def is_participant(self, lesson: Lesson, user: User) -> Optional[Lesson]:
