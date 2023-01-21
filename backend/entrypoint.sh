7d594b05f3cf368959b718683e831b675068caf4
Никита Есипов <dasaderto@gmail.com>
2023-01-21 12:04:04 +0300

    Add celery running in entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,4 +5,6 @@
 ./manage.py search_index --rebuild -f
 ./manage.py collectstatic --noinput
 
+celery -A server worker -c2 -l fatal &
+celery -A server beat -l info &
 gunicorn --chdir server --bind :8000 server.wsgi:application
