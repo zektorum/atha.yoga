16187c1725a01cac59f92831300f3ee55acfaf69
Никита Есипов <dasaderto@gmail.com>
2023-01-21 21:27:23 +0300

    Add base handlers classes for APIView and GenericAPIView

---
+++ backend/core/app/framework/handlers.py
@@ -0,0 +1,10 @@
+from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
+
+
+class Handler(APIView):
+    ...
+
+
+class GenericHandler(GenericAPIView):
+    ...
--- backend/courses/app/handlers/ticket_handlers.py
+++ backend/courses/app/handlers/ticket_handlers.py
@@ -6,15 +6,15 @@ from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
+from core.app.framework.handlers import Handler
 from core.app.framework.pagination import Pagination
 from courses.app.http.resources.ticket_resourses import TicketResource
 from courses.app.repositories.ticket_repository import TicketRepository
 
 
 @permission_classes([IsAuthenticated])
-class TicketListHandler(APIView):
+class TicketListHandler(Handler):
     @extend_schema(responses=OpenApiTypes.OBJECT)
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         repository = TicketRepository()
