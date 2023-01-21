7b443fa0dcf3d03abd165330ed1545e987c071ed
Никита Есипов <dasaderto@gmail.com>
2023-01-21 19:33:24 +0300

    Add wait for elastic search connection command

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,10 @@
 ./manage.py reset_db --noinput
 ./manage.py makemigrations
 ./manage.py migrate
+./manage.py wait_for_elasticsearch
 ./manage.py seed
 ./manage.py search_index --rebuild -f
 ./manage.py collectstatic --noinput
-./manage.py wait_for_elasticsearch
 celery -A server worker -c2 -l fatal &
 celery -A server beat -l info &
 gunicorn --chdir server --bind :8000 server.wsgi:application
